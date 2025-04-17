418ca6187b9a00fd3c4933eb9ed16da25e89bfb3
GiselleLop <gisellealex346@gmail.com>
2025-04-16 19:42:15 -0500

    components required added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import style from "./Header.module.css"
+
+export const Header = () => {
+  return (
+    <div className={style.header_main}>
+      HEADER
+    </div>
+  )}
---
+++ src/pages/DirectoryView/index.tsx
@@ -0,0 +1,8 @@
+import style from "./DirectoryView.module.css"
+
+export const DirectoryPage = () => {
+  return (
+    <section id="directory" data-route="/directory" className={style.directory_main}>
+        directory_main view
+    </section>
+  )}
