095fbfc96136c740661d319c118f52ec36fb7baf
GiselleLop <gisellealex346@gmail.com>
2025-04-16 20:46:37 -0500

    home first view added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,6 @@ import style from "./Header.module.css"
 export const Header = () => {
   return (
     <div className={style.header_main}>
-      HEADER
+      <h1 className={style.title_header}>Markdown Link Analyzer</h1>
     </div>
   )}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,25 @@
 import style from "./Home.module.css"
+import fileIcon from "../../assets/images/fileIcon.png"
+import directoryIcon from "../../assets/images/directoryIcon.png"
 
 export const HomePage = () => {
   return (
     <section id="home" data-route="/" className={style.home_main}>
-        homee
+       <h1 className={style.text_home}>Discover, manage, scan, and analyze all the links in your Markdown files quickly, easily, and with just one click.</h1>
+       <div className={style.selectors_container}>
+        
+        <div className={style.directory}>
+            <img className={style.icon_select} src={directoryIcon}/>
+            <h1 className={style.select_text}>Choose a directory</h1>
+        </div>
+
+        <div className={style.file}>
+            <img className={style.icon_select} src={fileIcon}/>
+            <h1 className={style.select_text}>Choose a MD file</h1>
+
+        </div>
+       
+       </div>
+       <button className={style.button_main}>Analyze</button>
     </section>
   )}
